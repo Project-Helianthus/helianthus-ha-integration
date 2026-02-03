972e2dd4a5c4736303225090bc47d5133b7da656
Răzvan Corneliu C.R. VILT <16434603+d3vi1@users.noreply.github.com.>
2026-02-03 13:52:06 +0200

    M1: add config flow for host/port

--- custom_components/helianthus/__init__.py
+++ custom_components/helianthus/__init__.py
@@ -1,1 +1,14 @@
 """Helianthus Home Assistant integration."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Helianthus from a config entry."""
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return True
